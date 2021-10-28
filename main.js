bf2bfc668474108adf2c31460e4956f176bf488d
viniciusfg05 <68232658+viniciusfg05@users.noreply.github.com>
2021-10-28 12:04:09 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -38,8 +38,19 @@ const swiper = new Swiper('.swiper-container', {
   pagination: {
     el: '.swiper-pagination'
   },
-  mousewheel: true,
-  keyboard: true
+  mousewheel: {
+    invert: true
+  },
+  keyboard: true,
+  autoplay: {
+    delay: 8000
+  },
+  breakpoints: {
+    767: {
+      slidesPerView: 2,
+      setWrapperSize: true
+    }
+  }
 })
 
 //scrollReveal - Mostra elemento quando der scroll na pagina
@@ -47,19 +58,86 @@ const swiper = new Swiper('.swiper-container', {
 const scrollReveal = ScrollReveal({
   origin: 'top',
   distance: '30px',
-  duration: 500,
+  duration: 300,
   reset: true
 })
 
 scrollReveal.reveal(
   `#home .text, #home .image,
-  #about .text, #about .image,
+  #about .image, #about .text,
   #services header, #services .card,
   #testimonials header, #testimonials .testimonial,
   #contact .text, #contact .button, #contact .links
+  footer .brand, footer .social
   
   `,
   {
     interval: 100
   }
 )
+
+//rolagem ao top
+
+const backToTopButton = document.querySelector('.back-to-top')
+window.addEventListener('scroll', function () {
+  if (window.scrollY > +560) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+})
+
+/* Menu Ativo conforme a seção visível na página*/
+
+/* Menu ativo conforme a seção visível na página */
+const sections = document.querySelectorAll('main section[id]')
+function activateMenuAtCurrentSection() {
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+
+  for (const section of sections) {
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+    const sectionId = section.getAttribute('id')
+
+    const checkpointStart = checkpoint >= sectionTop
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+
+    if (checkpointStart && checkpointEnd) {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.add('active')
+    } else {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.remove('active')
+    }
+  }
+}
+
+window.addEventListener('scroll', function () {
+  activateMenuAtCurrentSection()
+})
+
+/*const section = document.querySelectorAll('main section[id]')
+function activateMenuAtCurrentSection () {
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+
+  for (const section of sections){
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+    const sectionId = section.getAtribute('id')
+
+    const checkpointStart = checkpoint >= sectionTop
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+
+    if (checkpointStart = checkpoint) {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        classList.add('active')
+    } else {
+      document
+      .querySelector('nav ul li a[href*=' + sectionId + ']')
+      classList.remove('active')
+    }
+  }
+*/
